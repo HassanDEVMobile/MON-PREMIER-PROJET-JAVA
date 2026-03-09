cbed8a3baac97ae41558a972ce6235d2a82179b1
HassanDEVMobile <diabagatehassanilias@gmail.com>
2026-03-09 12:56:00 +0000

    DEBUT DE CONNEXION AVEC EquipeDAO

--- src/dao/EquipeDAO.java
+++ src/dao/EquipeDAO.java
@@ -6,40 +6,44 @@ import java.util.List;
 
 public class EquipeDAO {
 
-
+    private static String Equipe_INSERT = "INSERT INTO Equipe (id,Nom) VALUES (?,?)";
+    private static String Equipe_UPDATE = "UPDATE Equipe SET id = ? , Nom = ? WHERE id = ?";
+    private static String Equipe_FINDALL = "SELECT * FROM Equipe ";
+    private static String Equipe_FINDONE = "SELECT * FROM Equipe WHERE id = ";
+    private static String Equipe_DELETE = "DELETE FROM Equipe";
     /**
      * Ce DAO represente la logique de notre entiteEQUIPE
      * @author Diabagate
      * @Version 1.0
      */
-        /**
-         * Ctte methode permet de lister les equipes
-         * @return List<EQUIEPE>
-         */
-        public List<Equipe> findALL(){
-            return null;
-        }
+    /**
+     * Ctte methode permet de lister les equipes
+     * @return List<EQUIEPE>
+     */
+    public List<Equipe> findALL(){
+        return null;
+    }
 
-        /**
-         * Cette methode permet de supprimer une equipe a partir de son identifiant
-         * @param id ce paramettre represente l'identifiant d'une equipe
-         */
-        public void Delete(int id){
+    /**
+     * Cette methode permet de supprimer une equipe a partir de son identifiant
+     * @param id ce paramettre represente l'identifiant d'une equipe
+     */
+    public void Delete(int id){
 
-        }
+    }
 
-        /**
-         * Cette methode permet d'enregistrer une equipe
-         * @param name ce paramettre le nom de l'equipe
-         * @return EQUIPE
-         */
-        public Equipe add(String name){
+    /**
+     * Cette methode permet d'enregistrer une equipe
+     * @param name ce paramettre le nom de l'equipe
+     * @return EQUIPE
+     */
+    public Equipe add(String name){
 
-            return null;
-        }
+        return null;
+    }
 
-        public void update(String name){
+    public void update(String name){
 
-        }
+    }
 
 }
